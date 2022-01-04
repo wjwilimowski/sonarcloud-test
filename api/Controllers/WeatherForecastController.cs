5a89b999a6f175415091b6798c85e8133362aaee
Wojtek Wilimowski <wwilim@softserveinc.com>
2022-01-04 10:06:23 +0100

    Ugly commit

--- api/Controllers/WeatherForecastController.cs
+++ api/Controllers/WeatherForecastController.cs
@@ -21,12 +21,13 @@ public class WeatherForecastController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        var Weather = Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = DateTime.Now.AddDays(index),
             TemperatureC = Random.Shared.Next(-20, 55),
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        });
+
+        return Weather.ToArray();
     }
 }
